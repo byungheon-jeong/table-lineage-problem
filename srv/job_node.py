ea659be336bb38a4e27ad442eccabad3364b7cac
byungheon-jeong <45370147+byungheon-jeong@users.noreply.github.com>
2025-07-25 02:47:46 -0700

    save

--- srv/job_node.py
+++ srv/job_node.py
@@ -2,21 +2,23 @@ from collections import deque
 
 
 class JobNode():
-    def __init__(self, job_id:int, output_table:str, source_tables:list[str]=[], inheritors:set[str]=set([])):
+    def __init__(self, job_id:int, output_table:str, source_tables:list[str]=None, inheritors:set[str]=None):
         self.job_id = job_id    
         self.height = 1
         self.source_tables = source_tables
         self.output_name = output_table
-        self.inheritors_tables = inheritors
+        self.inheritors_tables = set()
 
 
-    def add_inheritors(self, node_inheritor):
-        self.inheritors_tables.add(node_inheritor)
-
-    def get_inheritors(self):
+    def get_inheritors(self) -> set:
         return self.inheritors_tables   
 
-    def get_output_name(self):
+
+    def add_inheritors(self, new_inheritor):
+        self.inheritors_tables.add(new_inheritor)
+
+    # This is the name of the table that the job outputs. This output_name (not job id) is the actual index that we are interested in
+    def get_output_name(self) -> str:
         return self.output_name
 
 
@@ -24,11 +26,11 @@ class JobNode():
         return self.job_id
         
 
-    def get_source_tables(self):
+    def get_source_tables(self) -> list:
         return self.source_tables
     
 
-    def get_height(self):
+    def get_height(self) -> int:
         return self.height
 
 
@@ -36,4 +38,3 @@ class JobNode():
         self.height = set_height
 
     
-
--- srv/job_tree.py
+++ srv/job_tree.py
@@ -1,25 +1,25 @@
 from srv.job_node import JobNode
 from collections import deque
-
+from collections import defaultdict
 class JobTree(): 
     def __init__(self, list_of_jobs: list[dict]):
         self.node_directory = {}
         self.cur_head = None
         self.job_source = {job["Output Table"]: job for job in list_of_jobs}
 
-        for job in list_of_jobs[1:]:
+        for job in list_of_jobs:
             self.add_job_get_height(job)
 
 
-    def get_table(self, name_of_table: str) -> JobNode:
+    def get_node(self, name_of_table: str) -> JobNode:
         return self.node_directory.get(name_of_table, None)
 
 
-    def get_job_name_of_table(self, source_table_name: str):
+    def get_job_name_of_table(self, source_table_name: str) -> dict:
         return self.job_source[source_table_name]
 
 
-    def bubble_up_to_support(self, node: JobNode):
+    def bubble_up_to_support(self, node: JobNode) -> None:
         dfs = deque()
     
         dfs.appendleft(node)
@@ -29,17 +29,12 @@ class JobTree():
             inheritors = node.get_inheritors()
             height = node.get_height() + 1
 
-            for heir in inheritors:
+            for heir in set(inheritors):
                 heir.set_height(height)
                 dfs.appendleft(heir)
-            
-        
 
-            
 
-    def add_job_get_height(self, job_info: dict):
-        bfs_node = deque()
-        
+    def add_job_get_height(self, job_info: dict) -> int:
         node = self.node_directory.get(job_info["Output Table"], None)
         
         if node:
@@ -48,6 +43,8 @@ class JobTree():
         # if node does not exist
         job_info = {"_".join(key.split(" ")).lower(): info for key,info in job_info.items()}
         node = JobNode(**job_info)
+        if node.output_name == "X":
+            print("AHHHH")
         self.node_directory[node.output_name] = node
 
         height_tracker = []
@@ -55,9 +52,10 @@ class JobTree():
         for source_table in node.get_source_tables():
             # BASE Case, exit
             if source_table == "SOURCE TABLES":
-                return 0
+                return 1
             
-            source_node = self.get_table(source_table)
+            source_node_name = self.get_job_name_of_table(source_table)
+            source_node = self.get_node(source_node_name["Output Table"])
             
             if source_node:
                 source_node.add_inheritors(node)
@@ -70,18 +68,87 @@ class JobTree():
         height = max(height_tracker)
         node.set_height(height)
 
-
+        self.bubble_up_to_support(node)
         
         # for node.get_source_tables():
         
-
-
-
         return height        
 
-    def get_similarity(self, node1:JobNode, node2:JobNode) -> float:
+    def calculate_similarity(self, node1:JobNode, node2:JobNode) -> float:
 
-        bfs = deque()
+
+        similarity_score = 0
+
+        bfs_a = deque()
+        bfs_b = deque()
+        
+        bfs_a.appendleft(node1)
+        bfs_b.appendleft(node2)
+
+        recursive_source_tables_a = defaultdict(set)
+        recursive_source_tables_b = defaultdict(set)
+
+        while bfs_a:
+            node = bfs_a.pop()
+            cur_height = node.get_height()
+            output_table_name = node.get_output_name()
+            recursive_source_tables_a[cur_height].add(output_table_name)
+            for table in node.get_source_tables():
+                bfs_a.appendleft(table)
+            
+        while bfs_b:
+            node = bfs_b.pop()
+            cur_height = node.get_height()
+            output_table_name = node.get_output_name()
+            recursive_source_tables_b[cur_height].add(output_table_name)
+            for table in node.get_source_tables():
+                bfs_b.appendleft(table)
+            
+        n1_height = node1.get_height()
+        n2_height= node2.get_height()
+        current_height = min(n1_height, n2_height)
+        max_height = max(n1_height, n2_height)
+
+        while current_height > 0:
+            # To weigh the different "levels" of the source table graph as more distant common tables should be counted for less
+            distance_multiplier = max_height - current_height
+
+            node_a_source_tables_at_cur_level = recursive_source_tables_a[cur_height]
+            node_b_source_tables_at_cur_level = recursive_source_tables_b[cur_height]
+            # We are counting the number of shared source tables of node_a and node_b at each "level"
+            number_of_common_source_tables = len(node_a_source_tables_at_cur_level & node_b_source_tables_at_cur_level)
+            
+            similarity_score += distance_multiplier * number_of_common_source_tables
+
+        return similarity_score
+
+
+        # while current_height > 0 and bfs_a and bfs_b:
+        #     source_tables_at_cur_level_a = set()
+        #     source_tables_at_cur_level_b = set()
+            
+        #     node_a = bfs_a.pop()
+        #     node_b = bfs_b.pop()
+
+        #     for source_table_names in node_a.get_source_tables():
+        #         job_node = self.get_node(source_table_names)
+        #         job_height = job_node.get_height()
+        #         if job_node.get_height() == current_height:
+        #             source_tables_at_cur_level_a.add(job_node.get_output_name())
+        #         elif job_node.get_height() < current_height:
+        #             bfs_a.pop()
+
+
+
+        # while bfs_a and bfs_b:
+        #     node1 = bfs_a.pop()
+        #     node2 = bfs_b.pop()
+        #     n1_height = node1.get_height()
+        #     n2_height= node2.get_height()
+
+
+
+
 
 
 
